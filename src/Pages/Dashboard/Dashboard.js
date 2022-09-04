4b00277c9d3a95f6c855c15abf01784e6227513e
nexxbaq <nexus.baquir@student.uts.edu.au>
2022-09-05 01:28:03 +1000

    Added css file for Dashboard and updated js file

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import styles from './Dashboard.module.css';
 
 const Dashboard = () => {
   return (
-    <div>
+    <div class>
+      <h1>General Notes and Dashboard</h1>
       <div className='input'>
         <input type='text' placeholder='New Note' />
       </div>
       <button>Add</button>
+      <div className={styles.note}>
+        <h3>Welcome Note</h3>
+        <p>"Hi, all. Welcome to the dashboard"</p>
+      </div>
+      <button>Delete</button>
     </div>
   );
 };
