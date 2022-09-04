46ac8052ddbe2f683889e0e308eda5a1ca04f2ca
nexxbaq <nexus.baquir@student.uts.edu.au>
2022-09-04 23:28:09 +1000

    Created a directory & js for dashboard page

---
+++ src/Pages/Dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Todo = () => {
+  return (
+    <div>
+      <div className='input'>
+        <input type='text' placeholder='New Note' />
+      </div>
+      <button>Add</button>
+    </div>
+  );
+};
+export default Todo;
