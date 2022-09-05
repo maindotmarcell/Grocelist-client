1605356a067ca03fb588765f08d3a18f2785f860
Symon <symonrahman20@gmail.com>
2022-09-05 11:45:45 +1000

    Personal todolist logic done

--- src/Pages/Todos/Todo.js
+++ src/Pages/Todos/Todo.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './TodoStyle.module.css';
 
 const Todo = () => {
+  const [todoitems, setTodoItems] = useState();
+  const [date, setDate] = useState();
+  const [todo, setTodo] = useState([]);
+  const handleTodo = () => {
+    console.log(todo);
+    setTodo((prevState) => [...prevState, { todoitems, date }]);
+  };
+  const handleChange = (e) => {
+    setTodoItems(e.target.value);
+  };
+  const handleDate = (e) => {
+    setDate(e.target.value);
+  };
   return (
-    <div>
-      <div className='input'>
-        <input type='text' placeholder='Grocery' />
-        <input type='text' placeholder='price' />
+    <>
+      <div>
+        <div className='input'>
+          <input type='text' placeholder='Grocery' onChange={handleChange} />
+          <input type='text' placeholder='price' onChange={handleDate} />
+        </div>
+        <button onClick={handleTodo}>Add</button>
       </div>
-      <button>Add</button>
-    </div>
+      <div className={styles.todoContainer}>
+        {todo.map((i) => {
+          return (
+            <div>
+              {i.todoitems}--{i.date}
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 export default Todo;
