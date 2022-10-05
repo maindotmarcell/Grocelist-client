60064bb4443cfe623b792741a346ebdec84fc0d8
Marcell Munnich <marcellmunnich@gmail.com>
2022-10-05 16:27:23 +1100

    Added test for AddGroupMember

--- src/App.test.js
+++ src/App.test.js
@@ -5,22 +5,31 @@ import CreateGroup from './Pages/__Dev-Test/CreateGroup';
 import AddGroupMember from './Pages/__Dev-Test/AddGroupMember';
 import Reminder from './Pages/Reminders/Reminders';
 test('renders the correct initial DOM', () => {
-  const doc = render(<Todo />);
+  const view = render(<Todo />);
 
-  const inputElement = doc.getByTestId('input');
+  const inputElement = view.getByTestId('input');
 
-  const todos = doc.queryAllByTestId('todo');
+  const todos = view.queryAllByTestId('todo');
 
   // The input should be blank.
   expect(inputElement.getAttribute('value')).toBe('');
 
-  // There should be 0 todos in the document.
+  // There should be 0 todos in the viewument.
   expect(todos.length).toBe(0);
 });
 
 test('render the correct intial DOM for Reminders', () => {
-  const doc = render(<Reminder />);
+  const view = render(<Reminder />);
 
-  const groupInputEl = doc.getByTestId('input');
+  const groupInputEl = view.getByTestId('input');
   expect(groupInputEl.getAttribute('value')).toBe('');
+});
+
+//Test for adding group members
+test('render the correct intial DOM for adding new users to groups', () => {
+  const view = render(<AddGroupMember />);
+  const groupInputlEl = view.getByTestId('groupID');
+  const userInputEl = view.getByTestId('userEmail');
+  expect(groupInputlEl.getAttribute('value')).toBe('');
+  expect(userInputEl.getAttribute('value')).toBe('');
 });
