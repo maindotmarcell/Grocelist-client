6c8a71c8feed5dfe21c258be4e5ae37673c6aafe
nexxbaq <nexus.baquir@student.uts.edu.au>
2022-10-06 23:30:27 +1100

    Added create notes functionality, modified css so it's uniform with the other pages

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,19 +1,48 @@
 import React from 'react';
+import { useState } from 'react';
 import styles from './Dashboard.module.css';
+import {IoAddCircleOutline, IoRemoveCircleOutline} from 'react-icons/io5'
 
 const Dashboard = () => {
+  const [dashnotes, setDashNotes] = useState('');
+  const [notes, setNotes] = useState([]);
+  
+  const handleDash = () => {
+    console.log(notes);
+    setNotes((prevState) => [...prevState, { dashnotes }]);
+  };
+
+  const handleChange = (e) => {
+    setDashNotes(e.target.value);
+  };
+ 
+  const handleDelete = () => {
+    console.log(dashnotes);
+    delete Dashboard.dashnotes;
+  }
+
   return (
-    <div >
-      <h1>General Notes and Dashboard</h1>
-      <div className='input'>
-        <input type='text' placeholder='New Note' />
+    <div className={styles.container}>
+      <h1>General Dashboard</h1>
+      <div>
+        <input className={styles.input} type='text' value={dashnotes} placeholder='Enter Note Text' onChange={handleChange}/>
       </div>
-      <button>Add</button>
+      <button className={styles.handleButton} onClick={handleDash}> <IoAddCircleOutline/> </button>
+
       <div className={styles.note}>
-        <h3>Welcome Note</h3>
-        <p>"Hi, all. Welcome to the dashboard"</p>
+        <h2>Group Notes</h2>
+        <div>
+        {notes.map((i) => 
+        {return (
+          <div className={styles.noteContent}>
+            {i.dashtitles}
+           ✏︎ {i.dashnotes}
+           <button className={styles.handleDelete} onClick={handleDelete}> <IoRemoveCircleOutline/> </button>
+
+          </div>
+        ); })}
       </div>
-      <button>Delete</button>
+      </div>
     </div>
   );
 };
