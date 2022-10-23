2267128717566314649f8788a32f68b7e6642641
Symon <symonrahman20@gmail.com>
2022-10-23 19:39:59 +1100

    update userDetails

--- src/Pages/Authentication/User.js
+++ src/Pages/Authentication/User.js
@@ -1,15 +1,45 @@
-import { useContext, useReducer } from 'react';
+import axios from 'axios';
+import { useContext, useReducer, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import UserContext from '../../context/UserContext';
 
 const User = () => {
   const { user } = useContext(UserContext);
+  const [edit, setEdit] = useState(false);
+  const [email, setEmail] = useState([]);
+  const [name, setName] = useState([]);
+
+  const handleEdit = () => {
+    setEdit(!edit);
+  };
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+  const handleSubmit = () => {
+    axios
+      .patch(`http://localhost:1337/api/authentication/${user.id}`, {
+        email,
+        name,
+      })
+      .then((response) => console.log(response.data));
+  };
   console.log(user);
   return (
     <div>
       {' '}
       <div>Useremail: {user.email} </div>
       <div>UserName: {user.name}</div>
+      <button onClick={() => handleEdit()}>Edit</button>
+      {edit ? (
+        <>
+          <input type='string' onChange={handleEmail} name='email' />
+          <input type='sting' onChange={handleName} name='name' />
+          <button onClick={handleSubmit}>Update</button>
+        </>
+      ) : null}
     </div>
   );
 };
