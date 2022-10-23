55efd2927e3abe83809652a0a200e99fce18f2f1
Marcell Munnich <marcellmunnich@gmail.com>
2022-10-23 23:38:54 +1100

    added tests for all existing features

--- src/App.test.js
+++ src/App.test.js
@@ -6,47 +6,140 @@ import AddGroupMember from './Pages/__Dev-Test/AddGroupMember';
 import Reminder from './Pages/Personal/Reminders/Reminders';
 import Dashboard from './Pages/Groups/Dashboard/Dashboard';
 import List from './Pages/Groups/List/List';
+import Login from './Pages/Authentication/Login/Login';
+import Groups from './Pages/Groups/Groups/Groups';
+import Invites from './Pages/Invites/Invites';
+import Registration from './Pages/Authentication/Registration/Registration';
+import GroupMenu from './Pages/Groups/GroupMenu/GroupMenu';
 import { useContext } from 'react';
+import UserContext, { UserProvider } from './context/UserContext';
+import { GroupProvider } from './context/GroupContext';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-// test('renders the correct initial DOM', () => {
-//   const view = render(<Todo />);
+test('renders the correct initial DOM', () => {
+  const view = render(
+ 		<Router>
+			<GroupProvider>
+				<UserProvider>
+					<Todo />
+				</UserProvider>
+			</GroupProvider>
+		</Router>
+  );
 
-//   const inputElement = view.getByTestId('input');
+  const inputElement = view.getByTestId('input');
 
-//   const todos = view.queryAllByTestId('todo');
+  const todos = view.queryAllByTestId('todo');
 
-//   // The input should be blank.
-//   expect(inputElement.getAttribute('value')).toBe('');
+  // The input should be blank.
+  expect(inputElement.getAttribute('value')).toBe('');
 
-//   // There should be 0 todos in the viewument.
-//   expect(todos.length).toBe(0);
-// });
+  // There should be 0 todos in the viewument.
+  expect(todos.length).toBe(0);
+});
 
 test('render the correct intial DOM for Reminders', () => {
-  const view = render(<Reminder />);
+	const view = render(<Reminder />);
 
-  const groupInputEl = view.getByTestId('input');
-  expect(groupInputEl.getAttribute('value')).toBe('');
+	const groupInputEl = view.getByTestId('input');
+	expect(groupInputEl.getAttribute('value')).toBe('');
 });
 
 //Test for adding group members
 test('render the correct intial DOM for adding new users to groups', () => {
-  const view = render(<AddGroupMember />);
-  const groupInputlEl = view.getByTestId('groupID');
-  const userInputEl = view.getByTestId('userEmail');
-  expect(groupInputlEl.getAttribute('value')).toBe('');
-  expect(userInputEl.getAttribute('value')).toBe('');
+	const view = render(<AddGroupMember />);
+	const groupInputlEl = view.getByTestId('groupID');
+	const userInputEl = view.getByTestId('userEmail');
+	expect(groupInputlEl.getAttribute('value')).toBe('');
+	expect(userInputEl.getAttribute('value')).toBe('');
 });
 
-// //Test for dashboard management page
-// test('render the correct intial DOM for dashboard', () => {
-//   const view = render(<Dashboard />);
-//   const groupInputlEl = view.getByTestId('todoContainer');
-//   expect(groupInputlEl.getAttribute('value')).toBe(null);
-// });
-// //Test for list page
-// test('render the correct to renders List', () => {
-//   const view = render(<List />);
-//   const inputElement = view.getByTestId('input');
-//   expect(inputElement.getAttribute('value')).toBe(null);
-// });
+test('render the correct intial DOM for groups/main page', () => {
+	const view = render(
+		<Router>
+			<GroupProvider>
+				<UserProvider>
+					<Login />
+				</UserProvider>
+			</GroupProvider>
+		</Router>
+	);
+}); 
+
+test('render the correct intial DOM for groups/main page', () => {
+	const view = render(
+		<Router>
+			<GroupProvider>
+				<UserProvider>
+					<Registration />
+				</UserProvider>
+			</GroupProvider>
+		</Router>
+	);
+}); 
+
+test('render the correct intial DOM for groups/main page', () => {
+	const view = render(
+		<Router>
+			<GroupProvider>
+				<UserProvider>
+					<Groups />
+				</UserProvider>
+			</GroupProvider>
+		</Router>
+	);
+}); 
+
+test('render the correct intial DOM for group menu page', () => {
+	const view = render(
+		<Router>
+			<GroupProvider>
+				<UserProvider>
+					<GroupMenu />
+				</UserProvider>
+			</GroupProvider>
+		</Router>
+	);
+});
+
+test('render the correct intial DOM for Invites page', () => {
+	const view = render(
+		<Router>
+			<GroupProvider>
+				<UserProvider>
+					<Invites />
+				</UserProvider>
+			</GroupProvider>
+		</Router>
+	);
+});
+
+//Test for dashboard management page
+test('render the correct intial DOM for dashboard', () => {
+	const view = render(
+		<Router>
+			<UserProvider>
+				<GroupProvider>
+					<Dashboard />
+				</GroupProvider>
+			</UserProvider>
+		</Router>
+	);
+	const groupInputlEl = view.getByTestId('todoContainer');
+	expect(groupInputlEl.getAttribute('value')).toBe(null);
+});
+
+//Test for list page
+test('render the correct to renders List', () => {
+	const view = render(
+		<Router>
+			<UserProvider>
+				<GroupProvider>
+					<List />
+				</GroupProvider>
+			</UserProvider>
+		</Router>
+	);
+	const inputElement = view.getByTestId('input');
+	expect(inputElement.getAttribute('value')).toBe(null);
+});
